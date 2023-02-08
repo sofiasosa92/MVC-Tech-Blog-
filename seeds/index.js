cdaea7f137b842ecf71e818cbdab679376147917
Sofia92 <sofiasosaperez@outlook.com>
2023-02-07 18:27:40 -0600

    fixed bug in seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -13,16 +13,15 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-const posts = await Post.findAll();
-
-
   for (const post of postData) {
     await Post.create({
       ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
-    //   post_id: posts[Math.floor(Math.random() * posts.length)].id,
     });
   }
+
+  const posts = await Post.findAll();
+
 
   for (const comment of commentData) {
     await Comment.create({
