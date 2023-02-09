1c88b31d11560ac1d9b3921187b1ece0a1b07f61
Sofia92 <sofiasosaperez@outlook.com>
2023-02-08 19:02:01 -0600

    utils done

---
+++ util/auth.js
@@ -0,0 +1,10 @@
+//Middleware to check if user is logged in
+const withAuth = (req, res, next) => {
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
