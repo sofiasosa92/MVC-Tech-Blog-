5ed60ff3320420d1df98ce5d15f55572f24e5b62
Sofia92 <sofiasosaperez@outlook.com>
2023-02-10 22:59:31 -0600

    worked on handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -3,21 +3,21 @@
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
 
-    if(email && password) {
+    if(username && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
-                email,
+                username,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
         });
 
         if(response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
