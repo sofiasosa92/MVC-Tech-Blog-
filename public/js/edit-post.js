e2b76d636ed6a4b2dbf10b3a7f7829dc29ba3cae
Sofia92 <sofiasosaperez@outlook.com>
2023-02-25 19:16:12 -0600

    working on edit post function

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,10 +1,11 @@
-//edit post function
+// edit post function
 
 async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value.trim();
     const post_text = document.querySelector('textarea[name="post-text"]').value.trim();
+
 
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length -1
@@ -21,6 +22,9 @@ async function editFormHandler(event) {
         }
     });
 
+    console.log(response); 
+
+
     if(response.ok) {
         document.location.replace('/dashboard/');
     } else {
@@ -29,3 +33,38 @@ async function editFormHandler(event) {
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+
+
+
+
+
+
+// async function editFormHandler(event) {
+//     event.preventDefault();
+
+//     const title = document.querySelector('input[name="post-title"]').value.trim();
+//     const post_text = document.querySelector('textarea[name="post-text"]').value.trim();
+
+//     const id = window.location.toString().split('/')[
+//         window.location.toString().split('/').length -1
+//     ];
+
+//     const response = await fetch(`/api/posts/${id}`, {
+//         method: 'PUT',
+//         body: JSON.stringify({
+//             title,
+//             post_text
+//         }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
+
+//     if(response.ok) {
+//         document.location.replace('/dashboard/');
+//     } else {
+//         alert(response.statusText);
+//     }
+// }
+
+// document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
